57f8927de4dd9c56f98238d1dd7bb55f8738d0f5
Jai Prakash <jprakashsingh198@gmail.com>
2024-08-09 14:16:32 +0530

    Backend Done !!

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
+import React from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import Login from "./Login";
-
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import toast from "react-hot-toast";
 function Signup() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state?.from?.pathname || "/";
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const userInfo = {
+      fullname: data.fullname,
+      email: data.email,
+      password: data.password,
+    };
+    await axios
+      .post("http://localhost:4001/user/signup", userInfo)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data) {
+          toast.success("Signup Successfully");
+          navigate(from, { replace: true });
+        }
+        localStorage.setItem("Users", JSON.stringify(res.data.user));
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err);
+          toast.error("Error: " + err.response.data.message);
+        }
+      });
+  };
   return (
     <>
       <div className="flex h-screen items-center justify-center">
         <div className=" w-[600px] ">
           <div className="modal-box">
-            <form  method="dialog">
+            <form onSubmit={handleSubmit(onSubmit)} method="dialog">
               {/* if there is a button in form, it will close the modal */}
               <Link
                 to="/"
@@ -25,10 +59,14 @@ function Signup() {
                   type="text"
                   placeholder="Enter your fullname"
                   className="w-80 px-3 py-1 border rounded-md outline-none"
-                
+                  {...register("fullname", { required: true })}
                 />
                 <br />
-               
+                {errors.fullname && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
               </div>
               {/* Email */}
               <div className="mt-4 space-y-2">
@@ -38,10 +76,14 @@ function Signup() {
                   type="email"
                   placeholder="Enter your email"
                   className="w-80 px-3 py-1 border rounded-md outline-none"
-                 
+                  {...register("email", { required: true })}
                 />
                 <br />
-              
+                {errors.email && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
               </div>
               {/* Password */}
               <div className="mt-4 space-y-2">
@@ -51,10 +93,14 @@ function Signup() {
                   type="text"
                   placeholder="Enter your password"
                   className="w-80 px-3 py-1 border rounded-md outline-none"
-                  
+                  {...register("password", { required: true })}
                 />
                 <br />
-                
+                {errors.password && (
+                  <span className="text-sm text-red-500">
+                    This field is required
+                  </span>
+                )}
               </div>
               {/* Button */}
               <div className="flex justify-around mt-4">
@@ -79,7 +125,7 @@ function Signup() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
